26f253ebb52118ef22b32b40c2845d0175f75066
Mateus de Castro <samuraiexx@gmail.com>
2019-04-19 19:04:40 -0300

    Added basic screen style using material-ui design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GenericTable from './components/GenericTable';
+import LeftMenu from './components/LeftMenu'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#424242",
+      contrastText: "#ffffff"
+    },
+    secondary: {
+      main: "#f1601d",
+      contrastText: "#ffffff"
+    },
+  },
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <LeftMenu>
+            <GenericTable />
+          </LeftMenu>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
