f29a48709e5d9f2313ca8c8bd4318ac948d3d3e0
Mateus de Castro <samuraiexx@gmail.com>
2019-04-20 12:27:23 -0300

    Remove link column

--- src/components/Problems/index.js
+++ src/components/Problems/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import GenericTable from '../GenericTable'
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
 
 const createDatum = (rawData) => {
   const datum = [];
@@ -18,13 +20,12 @@ const columns = [
   { id: 'description', numeric: false, label: 'Description' },
   { id: 'tags', numeric: false, label: 'tags' },
   { id: 'solution', numeric: false, label: 'Solution' },
-  { id: 'link', numeric: false, label: 'Problem Link' },
 ];
 
-const createLink = (link, label = 'Link') => <a target="_blank" rel="noopener noreferrer" href={link}>{label}</a>;
+const createLink = (link, label = 'Link') => <Typography><Link target="_blank" color="secondary" rel="noopener noreferrer" href={link}>{label}</Link></Typography>;
 
 const data = [
-  createDatum(['CodeForces', '203C', 'Adda and the array2', 4, 'HW2', 'Binary Search', 'asdfdf', createLink('https://google.com')]),
+  createDatum(['CodeForces', '203C', createLink('https://google.com', 'Adda and the array2'), 4, 'HW2', 'Binary Search', 'asdfdf']),
   createDatum(['CodeForces', '203B', 'Adda and the array', 4, 'HW2', 'Prefix sum; Bit; SegmentTree', 'asdfdf', 'asdfasdf']),
   createDatum(['CodeForces', '203D', 'Adda and the array3', 4, 'HW3', 'Prefix sum; Bit; SegmentTree', 'asdfdf', 'asdfasdf']),
   createDatum(['CodeForces', '203E', 'Adda and the array4', 4, 'HW5', 'Prefix sum; Bit; SegmentTree', 'asdfdf', 'asdfasdf']),
