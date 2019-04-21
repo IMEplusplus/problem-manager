603101df711d04e8b783f25abe6942e91cb3b44e
betrafi <arnonmellob4@hotmail.com>
2019-04-21 17:28:49 -0300

    Tag display with style and as chip

--- src/components/Problems/index.js
+++ src/components/Problems/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import GenericTable from '../GenericTable'
-import Link from '@material-ui/core/Link';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import GenericTable from "../GenericTable";
+import Link from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
+import Chip from "@material-ui/core/Chip";
+import { create } from "jss";
 
-const createDatum = (rawData) => {
+const createDatum = rawData => {
   const datum = [];
-  columns.forEach((column, i) => datum[column.id] = rawData[i]);
+  columns.forEach((column, i) => (datum[column.id] = rawData[i]));
   datum.key = createDatum.idCounter++;
 
   return datum;
@@ -13,26 +15,108 @@ const createDatum = (rawData) => {
 createDatum.idCounter = 0;
 
 const columns = [
-  { id: 'judge', numeric: false, label: 'Judge' },
-  { id: 'id', numeric: false, label: 'Id' },
-  { id: 'name', numeric: false, label: 'Name' },
-  { id: 'difficulty', numeric: true, label: 'Difficulty' },
-  { id: 'description', numeric: false, label: 'Description' },
-  { id: 'tags', numeric: false, label: 'tags' },
-  { id: 'solution', numeric: false, label: 'Solution' },
+  { id: "judge", label: "Judge" },
+  { id: "id", label: "Id" },
+  { id: "name", label: "Name" },
+  { id: "difficulty", label: "Difficulty" },
+  { id: "description", label: "Description" },
+  { id: "tags", label: "tags" },
+  { id: "solution", label: "Solution" }
 ];
 
-const createLink = (link, label = 'Link') => <Typography><Link target="_blank" color="secondary" rel="noopener noreferrer" href={link}>{label}</Link></Typography>;
+const createLink = (link, label = "Link") => (
+  <Typography>
+    <Link
+      target="_blank"
+      color="secondary"
+      rel="noopener noreferrer"
+      href={link}
+    >
+      {label}
+    </Link>
+  </Typography>
+);
+
+const style = {
+  background: 'FF8C00',
+  borderRadius: 10,
+  border: 0,
+  color: 'white',
+  height: 20,
+  padding: '0 0px',
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+};
+
+const createChips = tags => (
+  <div>
+    {tags.map(tag => (
+      <Chip className="mb1 mr1" style={style} label={tag} color="secondary" key={tag} />
+    ))}
+  </div>
+);
 
 const data = [
-  createDatum(['CodeForces', '203C', createLink('https://google.com', 'Adda and the array2'), 4, 'HW2', 'Binary Search', 'asdfdf']),
-  createDatum(['CodeForces', '203B', 'Adda and the array', 4, 'HW2', 'Prefix sum; Bit; SegmentTree', 'asdfdf', 'asdfasdf']),
-  createDatum(['CodeForces', '203D', 'Adda and the array3', 4, 'HW3', 'Prefix sum; Bit; SegmentTree', 'asdfdf', 'asdfasdf']),
-  createDatum(['CodeForces', '203E', 'Adda and the array4', 4, 'HW5', 'Prefix sum; Bit; SegmentTree', 'asdfdf', 'asdfasdf']),
-  createDatum(['CodeForces', '203E', 'Adda and the array4', 4, 'HW5', 'Prefix sum; Bit; SegmentTree', 'asdfdf', 'asdfasdf']),
-  createDatum(['CodeForces', '203E', 'Adda and the array4', 4, 'HW5', 'Prefix sum; Bit; SegmentTree', 'asdfdf', 'asdfasdf']),
+  createDatum([
+    "CodeForces",
+    "203C",
+    createLink("https://google.com", "Adda and the array2"),
+    4,
+    "HW2",
+    createChips(["Binary Search"]),
+    "asdfdf"
+  ]),
+  createDatum([
+    "CodeForces",
+    "203B",
+    "Adda and the array",
+    4,
+    "HW2",
+    createChips(["Prefix sum", "Bit", "SegmentTree"]),
+    "asdfdf",
+    "asdfasdf"
+  ]),
+  createDatum([
+    "CodeForces",
+    "203D",
+    "Adda and the array3",
+    4,
+    "HW3",
+    createChips(["Prefix sum", "Bit", "SegmentTree"]),
+    "asdfdf",
+    "asdfasdf"
+  ]),
+  createDatum([
+    "CodeForces",
+    "203E",
+    "Adda and the array4",
+    4,
+    "HW5",
+    createChips(["Prefix sum", "Bit", "SegmentTree"]),
+    "asdfdf",
+    "asdfasdf"
+  ]),
+  createDatum([
+    "CodeForces",
+    "203E",
+    "Adda and the array4",
+    4,
+    "HW5",
+    createChips(["Prefix sum", "Bit", "SegmentTree"]),
+    "asdfdf",
+    "asdfasdf"
+  ]),
+  createDatum([
+    "CodeForces",
+    "203E",
+    "Adda and the array4",
+    4,
+    "HW5",
+    createChips(["Prefix sum", "Bit", "SegmentTree"]),
+    "asdfdf",
+    "asdfasdf"
+  ])
 ];
 
 export default () => (
-  <GenericTable data={data} columns={columns} title="Problems"/>
+  <GenericTable data={data} columns={columns} title="Problems" />
 );
